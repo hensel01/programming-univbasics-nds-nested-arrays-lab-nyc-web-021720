0d87af061f78428ed488241c5a61fd9641192910
Eliot Henson <eliotrhenson@gmail.com>
2020-02-06 14:50:43 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,7 +33,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-matrix_lookup(sorted_matrix, 0, 0,)
+matrix_lookup(sorted_matrix, row, column,)
 end
 
 def matrix_update(matrix, row, column, new_value)
